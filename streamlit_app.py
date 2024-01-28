f29d786904588bc60e5a8e9fef2ba4f45446b40c
Hakan Yücetürk <57252113+hakan8252@users.noreply.github.com>
2024-01-28 21:04:13 +0300

    lgbm

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np
 from sklearn.model_selection import train_test_split
 import joblib
+import lightgbm
 
 # Streamlit App
 st.title('Telco Churn Prediction Dockerized App')
